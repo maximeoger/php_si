47805a9f5573b3323c9e0c854881d908aec63f33
Matt <Matt@MattBook.local>
2018-02-14 17:43:59 +0100

    ok

--- doadd_user.php
+++ doadd_user.php
@@ -2,14 +2,8 @@
 
 // d'abbord, on teste si les champs obligatoires ne sont pas existant if( !isset ) signifie : non égal à isset . si c'est le cas , il ajoute l'utilisateur à la bdd sinon il renvoie une erreur
 
-if (
-    !isset($_POST['name']) ||
-    !isset($_POST['lastname']) ||
-    !isset($_POST['birthdate']) ||
-    !isset($_POST['fav']) ||
-    !isset($_POST['email']) ||
-    !isset($_POST['password'])
-) {
+if (empty($_POST['name']) || empty($_POST['LASTNAME']) || empty($_POST['birthdate']) || empty($_POST['fav']) || empty($_POST['email']) || empty($_POST['password']))
+{
     header('Location: add_user.php?nopostdata');
     exit;
     // si l'un des champs est vide, il renvoie sur a page actuelle avec une erreur dans le header
@@ -18,9 +12,9 @@ if (
 require_once "connexion_db.php"; // on appelle la base de données
 
 $requette = " INSERT INTO `users`
- (`id`, `name`, `LASTNAME`, `birthdate`, `fav`, `email`, `password`)
+ (`name`, `LASTNAME`, `birthdate`, `fav`, `email`, `password`)
  VALUES
- (NULL, :nom, :lastname, :birthdate, :fav, :email, :password)
+ (:nom, :lastname, :birthdate, :fav, :email, :password)
 ;";
 
 //** la requette SQL çi dessus permet d'ajouter les valeurs des champs
@@ -28,12 +22,12 @@ $requette = " INSERT INTO `users`
 
 
 $stmt = $conn->prepare($requette);
-$stmt->bindValue(':nom', $_POST[name]);
-$stmt->bindValue(':lastname', $_POST[lastname]);
-$stmt->bindValue(':birthdate', $_POST[birthdate]);
-$stmt->bindValue(':fav', $_POST[fav]);
-$stmt->bindValue(':email', $_POST[email]);
-$stmt->bindValue(':password', $_POST[password]);
+$stmt->bindValue(':nom', $_POST['name']);
+$stmt->bindValue(':lastname', $_POST['LASTNAME']);
+$stmt->bindValue(':birthdate', $_POST['birthdate']);
+$stmt->bindValue(':fav', $_POST['fav']);
+$stmt->bindValue(':email', $_POST['email']);
+$stmt->bindValue(':password', $_POST['password']);
 $stmt->execute();
 
 
--- header.php
+++ header.php
@@ -1,14 +1,26 @@
 <?php
 
 require_once "connexion_db.php";
-
+$requete = "SELECT
+`name`,
+`LASTNAME`
+FROM
+`users`
+WHERE
+id = :id
+;";
+$stmt = $conn->prepare($requete);
+$stmt->bindValue(':id', $_GET['id']);
+$stmt->execute();
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
+
 <header class="header">
    <nav class="wrapper">
       <p class="logo"><span class="light">My</span>Sport</p>
       <input class="style-button-search" type="text" name="pseudo" id="pseudo" placeholder="Search"/>
       <div class="nav-right-content">
-         <strong><span class="light">Bonjour</span> John Doe</strong>
+         <strong><span class="light">Bonjour</span> <?=$row['name'].' '.$row['LASTNAME']?> </strong>
       </div>
    </nav>
 </header>
