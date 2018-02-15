9b6e94b372143f67c815281c0c1435d8ceee62a8
Maxime <maximeoger93@gmail.com>
2018-02-15 19:26:18 +0100

    Update connexion_db.php

--- connexion_db.php
+++ connexion_db.php
@@ -3,8 +3,11 @@
 session_start();
 //phpinfo();
 
-try {
-    $conn = new PDO('mysql:dbname=si_php_211802;host=127.0.0.1', 'root', 'root');
+try { 
+    $conn = new PDO('mysql:dbname=si_php_211802;host=127.0.0.1', 'root', 'root'); 
+    
+    // on charge la base de données via PDO. Cette expression est stockée dans une variable $conn
 } catch(PDOException $exception) {
     exit($exception -> getMessage());
-}
+    
+    // si le chargement échoue, on reçois un message d'erreur
