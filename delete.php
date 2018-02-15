a9797186552b01076a23d16b08ff41d2a05ea7c3
Maxime <maximeoger93@gmail.com>
2018-02-15 19:32:00 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,7 +1,9 @@
 <? php
 
 
-require_once "connexion_db.php";
+require_once "connexion_db.php"; // on récupère la bdd
+
+// on SUPPRIME l'ID et le CONTENT de la table post , ici la requette est juste définie dans une variable requette
 $requete = 'DELETE
   `id`,
   `content`,
@@ -11,9 +13,9 @@ $requete = 'DELETE
   id = :id
   ;';
 
-$stmt=$conn->prepare($requete);
-$stmt->bindValue(':id', $_GET['id']);
-$stmt->execute();
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$stmt=$conn->prepare($requete); // on prépare la requette
+$stmt->bindValue(':id', $_GET['id']); // on associe la valeur de :id par l'id du post à supprimé que l'on récupère en GET dans l'url
+$stmt->execute(); // on execute la requette
+$row = $stmt->fetch(PDO::FETCH_ASSOC); // on récupère le résultats de la requette pour les stocker dans $row
 
 ?>
