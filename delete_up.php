792ada59c4d75f8bf4bff7948e6b98e246462c94
yannis <yannikara01@gmail.com>
2018-02-15 17:23:38 +0100

    delete

--- delete_up.php
+++ delete_up.php
@@ -1,3 +1,15 @@
 <?php
 
+require_once "connexion_db.php";
+
+$requete= "DELETE FROM
+    `si_php_211802`
+    WHERE
+    id = :id;";
+
+$stmt = $conn->prepare($requete);
+$stmt->bindValue(':id',$_POST['id']);
+$stmt->execute();
+
+header('location: index.php');
 ?>
